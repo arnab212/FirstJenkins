a1fe5f700ff185805183d3fbfabcafbadc86cb95
arnab212 <70711617+arnab212@users.noreply.github.com>
2021-07-29 10:44:14 +0530

    Add project files.

---
+++ FirstJenkins/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstJenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
